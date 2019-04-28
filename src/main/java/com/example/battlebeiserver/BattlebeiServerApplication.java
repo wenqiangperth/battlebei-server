092f2d3efbc1b38b151ee2a77a6105933b3586c0
wenqiangperth <2280064372@qq.com>
2019-04-28 14:18:55 +0800

    init

---
+++ src/main/java/com/example/battlebeiserver/BattlebeiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.battlebeiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlebeiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattlebeiServerApplication.class, args);
+    }
+
+}
